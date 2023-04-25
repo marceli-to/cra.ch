2985fdec92420146bc20d41548fc09cabc13c026
Marcel Stadelmann <m@marceli.to>
2023-04-25 13:16:13 +0200

    wip

--- resources/views/components/galleries/gallery-link-image.blade.php
+++ resources/views/components/galleries/gallery-link-image.blade.php
@@ -1,4 +1,4 @@
 @props(['image', 'caption'])
-<a href="/img/cache/{{ $image->name }}/2000/{{ $image->coords }}" data-fancybox="gallery" data-caption="{{$caption}}">
+<a href="/img/cache/{{ $image->name }}/2000/{{ $image->coords }}" data-fancybox="gallery" data-caption="{{$caption}}" title="{{$caption}}">
   {{ $slot }}
 </a>
