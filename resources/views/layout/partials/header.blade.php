bcb45d23ee10078154c4bce9c6e707ac91887de1
Marcel Stadelmann <m@marceli.to>
2023-04-25 13:12:37 +0200

    wip

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -3,7 +3,7 @@
     <a href="{{ route('page.home') }}" class="logo" title="{{ __('Home') }}">
       <x-icons.logo />
     </a>
-    <a href="javascript:;" class="btn-menu" data-menu-btn>
+    <a href="javascript:;" class="btn-menu" data-menu-btn title="Menu anzeigen/verbergen">
       <x-icons.burger class="icon-menu-burger" />
       <x-icons.cross class="icon-menu-cross" />
     </a> 
