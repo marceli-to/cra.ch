525d1e746e67956c926adfc75935ddbbd3c80a82
Marcel Stadelmann <m@marceli.to>
2023-09-06 11:49:44 +0200

    wip

--- resources/views/components/project/worklist-sm.blade.php
+++ resources/views/components/project/worklist-sm.blade.php
@@ -4,7 +4,7 @@
     @if ($projects->count() > 0)
       <h2>{{ str_replace("<br />", ", ", nl2br($category->title)) }}</h2>
     @endif
-    @foreach ($category->projects as $project)
+    @foreach ($category->projects->sortByDesc('periode') as $project)
       <article class="stacked-item stacked-item--border">
         @if ($project->hasDetailPage)
           <a href="{{ route('page.project.show', ['project' => $project]) }}" title="{{ $project->title }}">
