4c3cffeb8c0230ff23e1855673114a43a6d23626
Marcel Stadelmann <m@marceli.to>
2023-09-12 08:59:51 +0200

    wip

--- resources/views/pages/about/diary.blade.php
+++ resources/views/pages/about/diary.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.web')
 @section('seo_title', 'Tagebuch • Über uns')
 @section('seo_description', 'Erkennen und erfassen, spüren und wahrnehmen, beobachten und zuhören. Das tun wir immer wieder. Im Alltag und an besonderen Tagen. Das Vertraute und das Unbekannte, die Farben und die Schatten, die Intimität und die Weite, das Kleine und das Grosse, die Gegensätze und der Zusammenklang. In jeder Situation suchen wir nach der Essenz, der Authentizität und nach dem, was sie letztlich einzigartig macht.')
-@section('og_image', asset('assets/img/og/cristina-rutz-architekten-og-team.jpg'))
+@section('og_image', asset('assets/img/og/cristina-rutz-architekten-og-tagebuch.jpg'))
 @section('content')
 @if ($diary)
   <section>
